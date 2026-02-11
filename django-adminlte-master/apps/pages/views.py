48e5f0265c1be0f09fee213b9eacba93474ea2a1
shrutiraskar1768-creator <shrutiraskar1768@gmail.com>
2026-02-11 01:49:15 -0500

    added folder

--- django-adminlte-master/apps/pages/views.py
+++ django-adminlte-master/apps/pages/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+
+
 
 # Create your views here.
 
+
+
 def index(request):
 
-    # Page from the theme 
+
+
+    # Page from the theme
+
     return render(request, 'pages/index.html')
